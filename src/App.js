d03253de55f74c2d333e6fa18b0dca8daad1489a
Sam <sam.fordham1@gmail.com>
2020-08-05 15:03:55 +0100

    Trying to get the buttons to work

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Calculator from './pages/home/index.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is my first react app!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Calculator />
     </div>
   );
 }
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,21 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Calculator extends React.Component {
+
+
+export default class Calculator extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      firstNumber: "",
-      secondNumber: "",
+        firstNumber: "",
+        secondNumber: "",
+        total: "",
     };
   }
+
+
   changeHandler = (event) => {
     let nam = event.target.name;
     let val = event.target.value;
-    this.setState({[nam]: val});
+    this.setState({[nam]: val}); 
+    
+  }
+
+  changeHandler(){
+    const add = () => {
+        console.log (this.state.firstNumber) + (this.state.secondNumber);
+    }
+        <button onClick= {this.handleClick} value = "+"> + </button>
+    
+    let subtract = () => {
+        console.log(this.state.firstNumber) - (this.state.secondNumber);
+      } 
+      <button onClick= {this.handleClick} value = "-"> - </button>
+    
+    let multiply = () => {
+        console.log(this.state.firstNumber) * (this.state.secondNumber);
+    }
+        <button onClick= {this.handleClick} value = "*"> x </button>
+    
+    let divide = () => {
+        console.log(this.state.firstNumber) / (this.state.secondNumber);
+    }
+        <button onClick= {this.handleClick} value = "/"> / </button>
   }
   render() {
     return (
+        <div>
       <form>
       <h1>Calculator {this.state.firstNumber} {this.state.secondNumber}</h1>
       <p>Enter first number:</p>
@@ -29,11 +58,16 @@ class Calculator extends React.Component {
         onChange={this.changeHandler}
       />
       </form>
+
+
+      
+
+      </div>
     );
   }
 }
 
-export default class KeyPad extends Component{
+/*class KeyPad extends Component{
     buttonPressed = (event) => {
         this.props.buttonPressed(event.target.name);
     };
@@ -51,8 +85,7 @@ export default class KeyPad extends Component{
             </div>
         )
     }
-}
+}*/
 
 
 
-ReactDOM.render(<Calculator />, document.getElementById('root'));
